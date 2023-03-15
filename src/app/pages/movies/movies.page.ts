ea83d9373d33f0c1a4af2e4f3162a42b9b520567
tcfabritius <tim.fabritius@skiff.com>
2023-03-15 18:13:35 +0200

    Movie list

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
 import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-movies',
@@ -8,12 +10,37 @@ import { MovieService } from 'src/app/services/movie.service';
 })
 export class MoviesPage implements OnInit {
 
-  constructor(private movieService: MovieService) { }
+  movies:any = [];
+  currentPage = 1;
+  imageBaseUrl = environment.images;
+
+  constructor(private movieService: MovieService, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
-    this.movieService.getTopRatedMovies().subscribe(res => {
+    this.loadMovies();
+  }
+
+  async loadMovies(event?: InfiniteScrollCustomEvent){
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading..',
+      spinner: 'bubbles',
+    });
+    await loading.present();
+    this.movieService.getTopRatedMovies(this.currentPage).subscribe(res => {
+      loading.dismiss();
+      this.movies.push(...res.results);
       console.log(res);
-    })
+
+      event?.target.complete();
+      if (event) {
+        event.target.disabled = res.total_pages ===this.currentPage;
+      }
+    });
+  }
+
+  loadMore(event: InfiniteScrollCustomEvent){
+    this.currentPage++;
+    this.loadMovies(event);
   }
 
 }
