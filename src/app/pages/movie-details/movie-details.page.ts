4fad9ca74fa403a669b378416ac427dfb4d96f6c
tcfabritius <tim.fabritius@skiff.com>
2023-03-16 11:16:15 +0200

    movie page and android platform

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
+  movie:any = null;
+  imageBaseUrl = environment.images;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private movieService: MovieService) { }
 
   ngOnInit() {
+    const id:any = this.route.snapshot.paramMap.get('id');
+    this.movieService.getMovieDetails(id).subscribe((res) => {
+      console.log(res);
+      this.movie = res;
+    });
+  }
+
+  openHomepage(){
+    window.open(this.movie.homepage);
   }
 
 }
